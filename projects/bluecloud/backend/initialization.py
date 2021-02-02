1494ec091016f32b1a97e6f83335ab08b24e62a0
Mattia <m.dantonio81@gmail.com>
2021-02-02 14:55:46 +0100

    Initial commit

---
+++ projects/bluecloud/backend/initialization.py
@@ -0,0 +1,12 @@
+# from restapi.utilities.logs import log
+from restapi.connectors import sqlalchemy
+
+class Initializer:
+    """
+    This class is instantiated just after restapi init
+    Implement the constructor to add operations performed one-time at initialization
+    """
+
+    def __init__(self, app=None):
+        # c = sqlalchemy.get_instance()
+        pass
