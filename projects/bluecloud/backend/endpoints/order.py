cd00be49b136e4ae2ab2b1e54cd22140405d1999
Mattia <m.dantonio81@gmail.com>
2021-02-12 09:46:25 +0100

    Added POST /orders input schema

--- projects/bluecloud/backend/endpoints/order.py
+++ projects/bluecloud/backend/endpoints/order.py
@@ -1,14 +1,39 @@
-from typing import List
+from typing import List, TypedDict
 
 from restapi import decorators
+from restapi.config import TESTING
 from restapi.models import Schema, fields
 from restapi.rest.definition import EndpointResource, Response
 from restapi.utilities.logs import log
 
 
+class DownloadType(TypedDict):
+    url: str
+    filename: str
+    order_line: str
+
+
+class Download(Schema):
+    # download url to get the data
+    url = fields.Url(required=True)
+    # the new filename for the datafile
+    filename = fields.Str(required=True)
+    # unique number for identification
+    order_line = fields.Str(required=True)
+
+
 class OrderInputSchema(Schema):
+    # Unique Id number for debugging and communication
+    request_id = fields.Str(required=True)
+    # Unique ID to identify the web-site user
     marine_id = fields.Str(required=True)
-    urls = fields.List(fields.URL(), required=True)
+    # Unique order number
+    order_number = fields.Str(required=True)
+    # List of downloads
+    downloads = fields.List(fields.Nested(Download), required=True)
+    # Used to test the endpoint without call back Maris
+    # During tests is automatically defaulted to True ( === TESTING)
+    debug = fields.Boolean(missing=TESTING)
 
 
 class Order(EndpointResource):
@@ -22,14 +47,24 @@ class Order(EndpointResource):
         summary="Create a new order by providing a list of URLs",
         responses={"202": "Order creation accepted"},
     )
-    def post(self, marine_id: str, urls: List[str]) -> Response:
+    def post(
+        self,
+        request_id: str,
+        marine_id: str,
+        order_number: str,
+        downloads: List[DownloadType],
+        debug: bool,
+    ) -> Response:
 
         log.info("Create a new order")
 
         log.info("Launch a celery task to download urls in the marine_id folder")
 
         # Assign with the uuid of newly created resource
-        task_id = "..."
+        if debug:
+            task_id = "Debug mode enabled"
+        else:
+            task_id = "..."
 
         return self.response(task_id)
 
