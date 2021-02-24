3ef0e9f47c926cbf6ed127ea83ffbbebdcc5ad96
Mattia <m.dantonio81@gmail.com>
2021-02-24 06:43:27 +0100

    Compatibility fix

--- projects/bluecloud/backend/initialization.py
+++ projects/bluecloud/backend/initialization.py
@@ -11,3 +11,7 @@ class Initializer:
     def __init__(self, app=None):
         # c = sqlalchemy.get_instance()
         pass
+
+    # This method is called after normal initialization if TESTING mode is enabled
+    def initialize_testing_environment(self):
+        pass
