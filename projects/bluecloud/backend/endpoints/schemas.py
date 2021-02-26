c5570160543026ac804df4e2b21092ccfbaf4613
Mattia <m.dantonio81@gmail.com>
2021-02-26 08:27:19 +0100

    Stub of make order celery task

---
+++ projects/bluecloud/backend/endpoints/schemas.py
@@ -0,0 +1,33 @@
+from typing import TypedDict
+
+from restapi.config import TESTING
+from restapi.models import AdvancedList, Schema, fields
+
+
+class DownloadType(TypedDict):
+    url: str
+    filename: str
+    order_line: str
+
+
+class Download(Schema):
+    # download url to get the data
+    url = fields.Url(required=True)
+    # the new filename for the datafile
+    filename = fields.Str(required=True)
+    # unique number for identification
+    order_line = fields.Str(required=True)
+
+
+class OrderInputSchema(Schema):
+    # Unique Id number for debugging and communication
+    request_id = fields.Str(required=True)
+    # Unique ID to identify the web-site user
+    marine_id = fields.Str(required=True)
+    # Unique order number
+    order_number = fields.Str(required=True)
+    # List of downloads
+    downloads = AdvancedList(fields.Nested(Download), required=True)
+    # Used to test the endpoint without call back Maris
+    # During tests is automatically defaulted to True ( === TESTING)
+    debug = fields.Boolean(missing=TESTING)
