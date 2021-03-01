6426c9ab68ce0a469623694d4669150a32057a8f
Mattia <m.dantonio81@gmail.com>
2021-03-01 16:20:02 +0100

    Added missing typing

--- projects/bluecloud/backend/customization.py
+++ projects/bluecloud/backend/customization.py
@@ -1,10 +1,9 @@
-from typing import Any, Dict, Tuple
+from typing import Any, Tuple
 
-from restapi.customizer import BaseCustomizer
+from restapi.customizer import BaseCustomizer, FlaskRequest, Props, User
+from restapi.rest.definition import EndpointResource
 
 # from restapi.utilities.logs import log
-
-Props = Dict[str, Any]
 
 
 class Customizer(BaseCustomizer):
@@ -14,9 +13,9 @@ class Customizer(BaseCustomizer):
     ) -> Tuple[Props, Props]:
 
         """
-            executed just before user creation
-            use this method to removed or manipulate input properties
-            before sending to the database
+        executed just before user creation
+        use this method to removed or manipulate input properties
+        before sending to the database
         """
         extra_properties: Props = {}
         # if "myfield" in properties:
@@ -24,26 +23,28 @@ class Customizer(BaseCustomizer):
         return properties, extra_properties
 
     @staticmethod
-    def custom_user_properties_post(user, properties, extra_properties, db):
+    def custom_user_properties_post(
+        user: User, properties: Props, extra_properties: Props, db: Any
+    ) -> None:
         """
-            executed just after user creation
-            use this method to implement extra operation needed to create a user
-            e.g. store additional relationships
+        executed just after user creation
+        use this method to implement extra operation needed to create a user
+        e.g. store additional relationships
         """
         pass
 
     @staticmethod
-    def manipulate_profile(ref, user, data):
+    def manipulate_profile(ref: EndpointResource, user: User, data: Props) -> Props:
         """
-            execute before sending data from the profile endpoint
-            use this method to add additonal information to the user profile
+        execute before sending data from the profile endpoint
+        use this method to add additonal information to the user profile
         """
         # data["CustomField"] = user.custom_field
 
         return data
 
     @staticmethod
-    def get_custom_input_fields(request, scope):
+    def get_custom_input_fields(request: FlaskRequest, scope: int) -> Props:
 
         # required = request and request.method == "POST"
         """
@@ -68,8 +69,8 @@ class Customizer(BaseCustomizer):
         return {}
 
     @staticmethod
-    def get_custom_output_fields(request):
+    def get_custom_output_fields(request: FlaskRequest) -> Props:
         """
-            this method is used to extend the output model of admin users
+        this method is used to extend the output model of admin users
         """
         return Customizer.get_custom_input_fields(request, scope=BaseCustomizer.ADMIN)
