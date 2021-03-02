8991de656afaa73ce550a35350a88b09f6be672d
Mattia <m.dantonio81@gmail.com>
2021-03-02 07:15:05 +0100

    Compatibility fix

--- projects/bluecloud/backend/initialization.py
+++ projects/bluecloud/backend/initialization.py
@@ -1,15 +1,10 @@
-from typing import Optional
-
-from restapi.customizer import FlaskApp
-
-
 class Initializer:
     """
     This class is instantiated just after restapi init
     Implement the constructor to add operations performed one-time at initialization
     """
 
-    def __init__(self, app: Optional[FlaskApp] = None) -> None:
+    def __init__(self) -> None:
         # c = sqlalchemy.get_instance()
         pass
 
