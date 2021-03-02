c9af75c58e3303ed90a5e29d2047029f4807ee94
Mattia <m.dantonio@cineca.it>
2021-03-02 12:34:00 +0100

    Added file download to celery task

--- projects/bluecloud/backend/tasks/make_order.py
+++ projects/bluecloud/backend/tasks/make_order.py
@@ -1,16 +1,27 @@
 from pathlib import Path
 from typing import List
 
+import requests
 from bluecloud.endpoints.schemas import DownloadType
 from restapi.connectors.celery import CeleryExt
 from restapi.exceptions import NotFound
 from restapi.services.uploader import Uploader
 from restapi.utilities.logs import log
 
+DOWNLOAD_HEADERS = {
+    "User-Agent": "BlueCloud DataCache HTTP-APIs",
+    "Upgrade-Insecure-Requests": "1",
+    "DNT": "1",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate",
+}
+
 
 @CeleryExt.task()
 def make_order(
     self: CeleryExt.TaskType,
+    request_id: str,
     marine_id: str,
     order_number: str,
     downloads: List[DownloadType],
@@ -22,15 +33,55 @@ def make_order(
     if not path.exists():
         raise NotFound(str(path))
 
-    with open(path.joinpath("test.success"), "w+") as f:
-        f.write("success!")
-
     log.info("Task ID: {}", self.request.id)
-
+    log.info("Request ID = {}", request_id)
     log.info("Marine ID = {}", marine_id)
     log.info("Order number = {}", order_number)
     log.info("Download list = {}", downloads[0:10])
 
+    response = {"request_id": request_id, "order_number": order_number, "errors": []}
+    for d in downloads:
+        download_url = d["url"]
+        filename = d["filename"]
+
+        log.info(download_url)
+        log.info(filename)
+        log.info(d["order_line"])
+
+        try:
+
+            r = requests.get(
+                download_url,
+                stream=True,
+                verify=False,
+                headers=DOWNLOAD_HEADERS,
+            )
+
+            local_path = path.joinpath(filename)
+
+            with open(local_path, "wb") as f:
+                for chunk in r.iter_content(chunk_size=1024):
+                    if chunk:  # filter out keep-alive new chunks
+                        f.write(chunk)
+
+        except requests.exceptions.ConnectionError as e:
+            log.error(str(e))
+            log.critical("ErrorCodes.UNREACHABLE_DOWNLOAD_PATH")
+            continue
+        except requests.exceptions.MissingSchema as e:
+            log.error(str(e))
+            log.critical("ErrorCodes.UNREACHABLE_DOWNLOAD_PATH")
+            continue
+        except BaseException as e:
+            log.error(str(e))
+            log.critical("ErrorCodes.UNKNOWN_DOWNLOAD_ERROR")
+            continue
+
+        if r.status_code != 200:
+            log.critical("ErrorCodes.UNREACHABLE_DOWNLOAD_PATH")
+            continue
+
     log.info("Task executed!")
 
+    print(response)
     return "Task executed!"
