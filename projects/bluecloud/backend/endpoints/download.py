ba350939f10b3ca29c1f297f6a5b139f50d1614a
Mattia <m.dantonio@cineca.it>
2021-03-08 12:59:13 +0100

    Changed download request method from post to get

--- projects/bluecloud/backend/endpoints/download.py
+++ projects/bluecloud/backend/endpoints/download.py
@@ -1,16 +1,5 @@
-from pathlib import Path
-from typing import Dict, List
-
 from restapi import decorators
-from restapi.exceptions import NotFound
-from restapi.models import Schema, fields
 from restapi.rest.definition import EndpointResource, Response
-from restapi.services.uploader import Uploader
-from restapi.utilities.logs import log
-
-
-class DownloadURLs(Schema):
-    urls = fields.List(fields.URL())
 
 
 class Download(EndpointResource):
@@ -26,27 +15,3 @@ class Download(EndpointResource):
         # send the file content
 
         return self.response("")
-
-    @decorators.auth.require()
-    @decorators.marshal_with(DownloadURLs, code=200)
-    @decorators.endpoint(
-        path="/download/<marine_id>/<order_number>",
-        summary="Request a download url for an order",
-        responses={"200": "Download URL(s) returned"},
-    )
-    def post(self, marine_id: str, order_number: str) -> Response:
-
-        path = Uploader.absolute_upload_file(order_number, subfolder=Path(marine_id))
-        log.info("Create a new order in {}", path)
-
-        if not path.exists():
-            raise NotFound(
-                f"Order {order_number} does not exists for marine id {marine_id}"
-            )
-
-        # invalidate previously created urls for this order
-
-        # create one or more urls and get back as response
-
-        data: Dict[str, List[str]] = {"urls": []}
-        return self.response(data)
