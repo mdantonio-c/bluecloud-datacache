a15d4d1cd6de3259e89a20173ce258c38f05e60a
Mattia <m.dantonio@cineca.it>
2021-03-30 15:32:41 +0200

    Bug fixes

--- projects/bluecloud/backend/endpoints/__init__.py
+++ projects/bluecloud/backend/endpoints/__init__.py
@@ -5,23 +5,24 @@ from cryptography.fernet import Fernet
 from restapi.config import APP_SECRETS
 from restapi.exceptions import BadRequest
 from restapi.services.authentication import import_secret
+from restapi.services.uploader import Uploader
 
 
-def get_seed(zip_path: str) -> bytes:
-    return import_secret(Path(zip_path).joinpath(".seed"))[0:12]
+def get_seed(abs_zip_path: Path) -> str:
+    return import_secret(abs_zip_path.joinpath(".seed"))[0:12].decode()
 
 
 def get_secret() -> bytes:
     return import_secret(APP_SECRETS.joinpath("order_secrets.key"))
 
 
-def get_token(zip_path: str, zip_name: str) -> str:
+def get_token(abs_zip_path: Path, zip_path: str) -> str:
 
     secret = get_secret()
     fernet = Fernet(secret)
 
-    seed = get_seed(zip_path)
-    plain = f"{seed}{os.path.join(zip_path, zip_name)}"
+    seed = get_seed(abs_zip_path)
+    plain = f"{seed}:{zip_path}"
 
     return fernet.encrypt(plain.encode()).decode()
 
@@ -30,12 +31,22 @@ def read_token(cypher: str) -> str:
     secret = get_secret()
     fernet = Fernet(secret)
 
-    # This is seed:zippath/filefile
+    # This is seed:marine_id/order_number/filefile
     plain = fernet.decrypt(cypher.encode()).decode().split(":")
+
+    # This is seed
     seed = plain[0]
+
+    # This is marine_id/order_number/filefile
     zip_filepath = plain[1]
+
+    # This is marine_id/order_number
     zip_path = os.path.dirname(zip_filepath)
-    expected_seed = get_seed(zip_path)
+
+    # This is /uploads/marine_id/order_number
+    abs_zip_path = Uploader.absolute_upload_file(zip_path)
+
+    expected_seed = get_seed(abs_zip_path)
 
     if seed != expected_seed:
         raise BadRequest("Invalid token seed")
