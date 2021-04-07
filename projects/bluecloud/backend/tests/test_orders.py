530b3b4c266f4d2a594829ffd40be769dba5261f
Mattia <m.dantonio@cineca.it>
2021-04-07 12:24:07 +0200

    # Tests fix

--- projects/bluecloud/backend/tests/test_orders.py
+++ projects/bluecloud/backend/tests/test_orders.py
@@ -9,7 +9,21 @@ import pytest
 from faker import Faker
 from restapi.services.uploader import Uploader
 from restapi.tests import API_URI, BaseTests, FlaskClient
-from tests import TemporaryRemovePath
+
+
+# This is a copy from submodules/do, not available in http-api
+class TemporaryRemovePath:
+    def __init__(self, path: Path):
+        self.path = path.absolute()
+        self.tmp_path = self.path.with_suffix(f"{path.suffix}.bak")
+
+    def __enter__(self):
+
+        self.path.rename(self.tmp_path)
+        return self
+
+    def __exit__(self, _type, value, tb):
+        self.tmp_path.rename(self.path)
 
 
 def download_and_verify_zip(
