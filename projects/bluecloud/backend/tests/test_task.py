7274a18a98813b5069e9ad6fbb921dc806af1624
Mattia <m.dantonio@cineca.it>
2021-04-19 08:52:44 +0200

    Extended task tests

--- projects/bluecloud/backend/tests/test_task.py
+++ projects/bluecloud/backend/tests/test_task.py
@@ -1,10 +1,10 @@
-# from pathlib import Path
+from pathlib import Path
+
 # from typing import Any, Dict, Optional, Type, TypeVar
 from faker import Faker
 from flask import Flask
+from restapi.services.uploader import Uploader
 from restapi.tests import BaseTests
-
-# from restapi.services.uploader import Uploader
 
 TASK_NAME = "make_order"
 
@@ -16,10 +16,10 @@ class TestApp(BaseTests):
         marine_id = faker.pystr()
         order_number = faker.pystr()
 
-        output = BaseTests.send_task(
+        response = BaseTests.send_task(
             app, TASK_NAME, request_id, marine_id, order_number, [], True
         )
-        assert output is None
+        assert response is None
 
         mail = BaseTests.read_mock_email()
 
@@ -31,3 +31,24 @@ class TestApp(BaseTests):
         assert body is not None
         assert "NotFound" in body
         assert f"{marine_id}/{order_number}" in body
+
+        path = Uploader.absolute_upload_file(order_number, subfolder=Path(marine_id))
+        path.mkdir(parents=True)
+
+        response = BaseTests.send_task(
+            app, TASK_NAME, request_id, marine_id, order_number, [], True
+        )
+
+        assert response is not None
+
+        assert "request_id" in response
+        assert response["request_id"] == request_id
+
+        assert "order_number" in response
+        assert response["order_number"] == order_number
+
+        assert "errors" in response
+        assert len(response["errors"]) == 0
+
+        # Nothing to be download => no errors and no zip output
+        assert not Path(path.joinpath("output.zip")).exists()
